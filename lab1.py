41e46929ffce5609a9845674ee51c0b749e92e85
reijiken <quyduongtran0810@gmail.com>
2022-03-24 21:04:09 +0700

    lab1

--- lab1.py
+++ lab1.py
@@ -1,37 +1,37 @@
-def getStudentCount ():
+def s ():
     return int (input ("Enter number of students: "))
     
-def getCourseCount ():
+def d ():
     return int (input ("Enter number of courses: "))
     
-def getCourseDetails ():
+def e ():
     cid = input ("Enter course id: ")
     cname = input ("Enter course name: ")
     return cid, cname
     
-def getStudentDetails ():
+def h ():
     sid = input ("Enter student id: ")
     sname = input ("Enter student name: ")
-    sdob = input ("Enter student's date of birth: ")
+    sdob = input ("Enter student's dob: ")
     return sid, sname, sdob 
     
-def getMarks (sid, cid):
+def m (sid, cid):
     prompt = f"Enter marks for student {sid} for course {cid}: ".format (sid, cid)
     input (prompt)
     
-nStudents = getStudentCount ()
+nStudents = s ()
 studentLst = []
 for i in range (nStudents):
-    sid, nm, dob = getStudentDetails ()
+    sid, nm, dob = d ()
     studentLst.append ((sid, nm, dob))
 
-nCourses = getCourseCount ()
+nCourses = e ()
 courseLst = []
 for i in range (nCourses):
-    cid, cname = getCourseDetails ()
+    cid, cname = h ()
     courseLst.append ((cid, cname))
     
-d = {}    
+p = {}    
 n = int (input ("Enter how many student-course marks do you want to enter? "))
 for i in range (n):
     while True:
@@ -45,20 +45,20 @@ for i in range (n):
             continue 
         break
     marks = int (input ("Enter marks: "))
-    if cid in d:
-        d [cid].append ((sid, marks))
+    if cid in p:
+        p [cid].append ((sid, marks))
     else:
-        d [cid] = [(sid, marks)]
+        p [cid] = [(sid, marks)]
         
 print ("\nListing all students now..")
 for s in studentLst:
-    print (f"Student id: {s[0]} Name: {s[1]} Date of birth: {s[2]}")
+    print (f"Student id: {s[0]} Name: {s[1]} Dob: {s[2]}")
 
 print ("\nListing all courses now..")
 for c in courseLst:
     print (f"Course id: {c[0]} Name: {c[1]}")
     
 cid = input ("\nWhich course do you want to see all student marks for? ")
-if cid in d:
+if cid in p:
     for tups in d [cid]:
         print (f"Student {tups[0]} got {tups [1]} marks")
