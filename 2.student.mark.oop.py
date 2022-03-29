5df8c2215668e1b923f0c79a9052c873a029092d
reijiken <quyduongtran0810@gmail.com>
2022-03-29 21:57:51 +0700

    2.student.mark.oop

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,71 +1,101 @@
+Student=[]
+StudentID=[]
+Course=[]
+CourseID=[]
+class course:
+    def __init__(self,id,name):
+        self.id=id
+        self.name=name
+        self.student=[]
+
+    def get_id(self):
+        return self.id
+    def get_name(self):
+        return self.name
+
 class student:
-    def __init__(self,id, name, dob,mark):
+    def __init__(self,id, name, dob,course_enter,mark):
        self.id=id
        self.name=name
        self.dob=dob
+       self.course_enter=course_enter
        self.mark=mark
-    def display(self, d):
-            print("Name   : ", d.name)
-            print("RollNo : ", d.id)
-            print("Date of birth : ", d.dob)
-            print("Marks : ", d.mark)
-            print("\n")
-    def accept(self, id, name, mark,dob ):
-        d = student(id, name, mark,dob)
-        lst.append(d)
-    def search(self, a):
-        for i in range(lst.__len__()):
-            if(lst[i].id == a):
-                return i       
-                                  
-    def delete(self, a):
-        i = obj.search(a)  
-        del lst[i]
-  
-    def update(self, a, New):
-        i = obj.search(a)
-        id = New
-        lst[i].id = id
-          
-class course:
-    def __init__(self,name,last_student):
-        self.name=name
-        self.last_student=last_student
-        self.student=[]
 
-    def input_student(self,student):
-        if len(self.student)<self.last_student:
-            self.student.append(student)
-            return True
-        return False
+    def get_id(self):
+        return self.id
+    def get_name(self):
+        return self.name
+    def get_dob(self):
+        return self.dob  
+         
+def number_course():
+    number_course=int(input("Enter the number of course:"))
+    if(number_course<=0):
+        print("error")
+        return 0
+    else:
+        return number_course
+        
+def add_course():
+    name=input("Enter the name course:")
+    id=input("Enten the ID course: ")
+    course_f={
+        'name':name,
+        'id':id
+    }
+    Course.append(course_f)
+    CourseID.append(id)        
     
-lst=[]
-obj = student( 0,'', 0,0)
-print("enter id,name and mark ")
-obj.accept( 1,"D",30_10, 100)
-obj.accept( 2,"E",9_8, 90)
-obj.accept( 3,"F",10_6, 80) 
+def inputstudent():
+    student_class=int(input("Enter the number of student in class:"))
+    if(student_class<=0):
+        print("error")
+        return 'False'
+    else:
+        return student_class
 
-print("\n")
-print("\nList of Students\n")
-for i in range(lst.__len__()):    
-    obj.display(lst[i])
+def add_student_infor():
+    print("enter information of student:")
+    
+    id=input("Enter the ID:")
+    name=input("Enter the student name:")
+    dob=input("Enter the dob student:")
+    course_enter=input("Enter the id course that student in:")
+    mark=input("Enter student mark:")
+    st_infor={
+        'id':id,
+        'name':name,
+        'dob':dob,
+        'course_enter':course_enter,
+        'mark':mark
+    }
+    StudentID.append(id)
+    Student.append(st_infor)
 
-print("\n Student Found, ")
-s = obj.search(2)
-obj.display(lst[s])
+def show_course():
+    print("course list")
+    for i in range(0,len(Course)):
+        print(f"ID:{Course[i]['id']}  name : {Course[i]['name']}")
 
-obj.delete(2)
-print(lst.__len__())
-print("List after deletion")
-for i in range(lst.__len__()):    
-    obj.display(lst[i])
+def show_student():
+    print("Student list ")
+    for i in range(0,len(Student)):
+        print(f"ID:{Student[i]['id']} name:{Student[i]['name']} DoB:{Student[i]['dob']} Courseid:{Student[i]['course_enter']} Mark:{Student[i]['mark']}")
 
-obj.update(3, 2)
-print(lst.__len__())
-print("List after updation")
-for i in range(lst.__len__()):    
-    obj.display(lst[i])
+j=int(number_course())
+m=1
+while m<=j:
+    m+=1
+    add_course()
+show_course()
+
+b=int(inputstudent())
+z=1
+while z<=b: 
+    z+=1
+    add_student_infor()
+show_student()
+
 
 
 
